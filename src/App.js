74c31e2a48e0f684a3f79c52bfdfb7143da8db4f
Blackcamel887 <Pajamas887.com>
2023-09-04 01:11:16 -0500

    make basic layout and functionality of game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+let ans = Math.floor(Math.random()*100)+1;//the answer to check on between 1 and 100 inclusive
 function App() {
+  let [userInput,setUserInput] = useState();//the holder for the userInput
+  let [outputMessage,setOutputMessage] = useState('?');//the holder for the message for user
+  let [gameState,setGameState] = useState('default');
+  let [buttonMessage,setButtonMessage] = useState('Check')
+  //the functionality to control the users input
+  function handleInputChange(e){
+    //makes sure user value is in allowed range
+    if(!e.target.value||(e.target.value<=100&&e.target.value>=1)){
+      setUserInput(e.target.value);
+    }
+  }
+  //the functionality that determines the appropriate message to be given to user
+  function handleOnClick(){
+    if(gameState==='default'){
+      if(userInput<ans){
+        console.log(ans);
+        setOutputMessage('Too small!');
+      }
+      else if(userInput===`${ans}`){
+        console.log('hi');
+        setOutputMessage('Bingo!');
+        setButtonMessage('Play again');
+        setGameState('winner');
+      }
+      else if(userInput>ans){
+        setOutputMessage('Too big!');
+      }
+    }
+    else if(gameState==='winner'){
+      setButtonMessage('Check');
+      ans = Math.floor(Math.random()*100)+1;//creates a new answer to check on between 1 and 100 inclusive
+      setOutputMessage('?');
+      setGameState('default');
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+      Number Guesser
+      </h1>
+      <h2>
+        Pick a number between 1-100
+      </h2>
+      <div className='top part'>
+        <text >
+          {outputMessage}
+        </text>
+      </div>
+      <input type = 'number' value={userInput} onChange = {handleInputChange} placeholder='Guess' max={100} min={1}>
+          
+      </input>
+      <br></br>
+      <button onClick = {handleOnClick}>
+        {buttonMessage}
+      </button>
     </div>
+
   );
 }
 
