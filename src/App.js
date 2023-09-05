a631571faddba943d8c9c5353f55930c9152f9e0
Blackcamel887 <Pajamas887.com>
2023-09-04 20:38:29 -0500

    add scoring system and fix minor issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import './App.css';
 let ans = Math.floor(Math.random()*100)+1;//the answer to check on between 1 and 100 inclusive
+let trackBest = 100;
 function App() {
   let [userInput,setUserInput] = useState();//the holder for the userInput
   let [outputMessage,setOutputMessage] = useState('?');//the holder for the message for user
   let [gameState,setGameState] = useState('default');//game state holder
-  let [buttonMessage,setButtonMessage] = useState('Check')
+  let [buttonMessage,setButtonMessage] = useState('Check')//the holder for the message that appears
+  let [currentScore,setCurrentScore] = useState(0);//holder for the current score
+  let [bestScore,setBestScore] = useState();//holder for the best score
   //the functionality to control the users input
   function handleInputChange(e){
     //makes sure user value is in allowed range
@@ -15,27 +18,40 @@ function App() {
   }
   //the functionality that determines the appropriate message to be given to user
   function handleOnClick(){
+    setCurrentScore(prev=>prev+1);
+    //increases current score with each click on button
     if(gameState==='default'){
-      if(userInput<ans){
-        console.log(ans);
+      if(userInput<ans){//switches back to default state
         setOutputMessage('Too small!');
+        setTimeout(()=>{
+          setOutputMessage('?');
+        },1250)
       }
       //switches gameState and updates correspoding messages
       else if(userInput===`${ans}`){
-        console.log('hi');
         setOutputMessage('Bingo!');
         setButtonMessage('Play again');
+        //changes best attempt 
+        if(currentScore+1<trackBest){
+          trackBest = currentScore+1;
+        }
+        setBestScore(trackBest);
         setGameState('winner');
       }
       else if(userInput>ans){
         setOutputMessage('Too big!');
+        setTimeout(()=>{
+          setOutputMessage('?');
+        },1250)
       }
     }
     //revert back all the states back to default state
     else if(gameState==='winner'){
       setButtonMessage('Check');
       ans = Math.floor(Math.random()*100)+1;//creates a new answer to check on between 1 and 100 inclusive
+      setUserInput('');
       setOutputMessage('?');
+      setCurrentScore(0);
       setGameState('default');
     }
   }
@@ -59,6 +75,7 @@ function App() {
       <button onClick = {handleOnClick}>
         {buttonMessage}
       </button>
+      <h3>Current tries: {currentScore} <br></br>Best attempt: {bestScore}</h3>
     </div>
 
   );
